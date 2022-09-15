f06b16d29206302c6aee6c66d0b7b3a1bdcff688
Tofuzinha <midoriihsan@gmail.com>
2022-09-15 17:43:53 -0300

    initial commit

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,10 @@
+export default {
+    title: "dark",
+    colors: {
+      primary: "#333",
+      secundary: "#f6511d",
+  
+      background: "#222",
+      text: "#fff",
+    },
+  };
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,10 @@
+export default {
+  title: "light",
+  colors: {
+    primary: "#f6511d",
+    secundary: "#ffb400",
+
+    background: "#f5f5f5",
+    text: "#333",
+  },
+};
