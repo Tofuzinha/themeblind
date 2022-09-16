53d506c02bad2105092091a21049d020ca5a27e5
Tofuzinha <midoriihsan@gmail.com>
2022-09-16 04:37:46 -0300

    jira001 feat: meu outro commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,7 +13,7 @@ const Header: React.FC<Props> = ({toggleTheme}) => {
 
   return (
     <Container>
-      Happy day Felix!!!
+      Happy day Lee Felix!!!
       <Switch 
       onChange={toggleTheme} 
       checked={title === 'dark'} 
